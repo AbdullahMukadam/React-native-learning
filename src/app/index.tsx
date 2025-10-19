e345cbf0ae5b1fbc736ceb5ad09b7dcd3baa0918
AbdullahMukadam <abdullahmukadam21@gmail.com>
2025-10-19 11:17:24 -0700

    completed the setup

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View style={{
+      backgroundColor: "black"
+    }}>
+      <Text style={{
+        textShadowColor:"green"
+      }}>index</Text>
+    </View>
+  )
+}
+
+export default index
